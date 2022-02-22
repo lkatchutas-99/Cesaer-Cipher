2d00843d77132befbe8f9a26aa410cdd75250f97
lkatchutas-99 <96667853+lkatchutas-99@users.noreply.github.com>
2022-02-21 23:36:12 -0500

    edit ceasaer_cipher.rb

--- cesaer_cipher.rb
+++ cesaer_cipher.rb
@@ -1,13 +1,13 @@
 print 'This is Caesar Cipher, enter any text: '
 text = gets
 
-print 'Ok, now enter a number: '
+print 'Ok, now enter a number, negative numbers will shift the letter up the alphabet: '
 shift_by_num = gets
 
 i = 0
 
 until /[0-9]/.match(shift_by_num) || i == 5
-  print 'Must enter a number: '
+  print "Must enter a number, #{5-i} until we kick you out: "
   shift_by_num = gets
   i += 1
   if i == 5
@@ -19,21 +19,38 @@ end
 shift_by_num = shift_by_num.to_i
 
 def ceasar_cipher(txt, shift)
-  if shift
+  unless shift == nil
+
     cipher = txt.split('').reduce(Array.new()) do |arr, letter|
-      for i in (0...shift)
-        if letter == 'z'
-        letter = 'a'
-        elsif letter == 'Z'
-          letter = 'A'
+      unless /[^a-zA-Z0-9]/.match(letter)
+        if shift < 0
+          for i in (0...(26+shift))
+            if letter == 'z'
+              letter = 'a'
+            elsif letter == 'Z'
+              letter = 'A'
+            else
+              letter = letter.next
+            end
+          end
         else
-          letter = letter.next
+          for i in (0...shift)
+            if letter == 'z'
+              letter = 'a'
+            elsif letter == 'Z'
+              letter = 'A'
+            else
+              letter = letter.next
+            end
+          end
         end
       end
-      arr.push(letter)
+      unless /[^a-zA_Z0-9]/.match(letter)
+        arr.push(letter)
+      end
       arr
     end.join
-    #puts cipher
+    puts cipher
     cipher
   end
 end
